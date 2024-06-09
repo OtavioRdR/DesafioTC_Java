b52a447dfb7b561f5167d64bdbc76556b05074cd
OtavioRdR <otaviorosa.rdr@gmail.com>
2024-06-09 19:01:30 -0300

    commit 4 OK

---
+++ src/main/java/com/meli/otavio/OtavioTransacaoApp.java
@@ -0,0 +1,12 @@
+package com.meli.otavio;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OtavioTransacaoApp {
+
+    public static void main(String[] args) {
+        SpringApplication.run(OtavioTransacaoApp.class, args);
+    }
+}
