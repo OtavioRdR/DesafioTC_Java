ea83ede3fcd22ee060b073f2a1e02553bb472c81
OtavioRdR <otaviorosa.rdr@gmail.com>
2024-06-09 19:01:22 -0300

    commit 3 OK

---
+++ src/main/java/com/meli/otavio/model/Estatistica.java
@@ -0,0 +1,14 @@
+package com.meli.otavio.model;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+
+@Data
+@AllArgsConstructor
+public class Estatistica {
+    private long count;
+    private double sum;
+    private double avg;
+    private double min;
+    private double max;
+}
